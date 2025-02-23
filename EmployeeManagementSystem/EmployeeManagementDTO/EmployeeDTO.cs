db7a8c93eabe606f1894a0288cee7b10f0d727d2
Sheikh Abdullah Arshad <abdullah.arshad1823@gmail.com>
2025-02-23 12:09:00 +0500

    Add files via upload

---
+++ EmployeeManagementSystem/EmployeeManagementDTO/EmployeeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementDTO
+{
+    public class EmployeeDTO
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Department { get; set; }
+        public double Salary { get; set; }
+        public string? Joindate { get; set; }
+
+    }
+}
